55ecea74a2551a09e1b27a50586e966afe51edde
Kaleab Beteselassie <54122528+Komputer24@users.noreply.github.com>
2024-06-23 00:24:12 -0400

    Scrambler App Layout

--- app/src/main/java/com/example/scrambleapp/MainActivity.kt
+++ app/src/main/java/com/example/scrambleapp/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.scrambleapp.ui.theme.ScrambleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +49,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Scrambler App",
-        modifier = modifier
-    )
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(
+            text = "Scrambler App",
+            fontSize = 30.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+        Text(
+            text = "by Kaleab Beteselassie",
+            fontSize = 10.sp,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.binary),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(text = "0")
+        Button(onClick = {
+
+        }){
+            Text(text = "Click Me!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
