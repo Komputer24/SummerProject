d46798096dba0ece1a0f90addf07c14d576505eb
Kaleab Beteselassie <54122528+Komputer24@users.noreply.github.com>
2024-06-23 13:44:59 -0400

    List Adder

--- app/src/main/java/com/example/scrambleapp/MainActivity.kt
+++ app/src/main/java/com/example/scrambleapp/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.benchmark.perfetto.Row
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,6 +38,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.scrambleapp.ui.theme.ScrambleAppTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,8 +48,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScrambleAppTheme {
-                var count = remember{
-                    mutableStateOf(0)
+                var name by remember{
+                    mutableStateOf("")
+                }
+                var names by remember{
+                    mutableStateOf(listOf<String>())
                 }
                 Column (
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -55,22 +69,30 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .padding(10.dp)
                     )
-                    Image(
-                        painter = painterResource(id = R.drawable.binary),
-                        contentDescription = null,
-                        contentScale = ContentScale.FillWidth
-                    )
-                    Text(
-                        text = count.value.toString()
-                    )
-                    Button(onClick = {
-                        count.value++
-                    }){
-                        Text(text = "Click Me!")
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = {
+                                text -> name = text
+                            }
+                        )
+                        Button(onClick = {
+                            if(name.isNotBlank()){
+                                names = names + name
+                            }
+                        }){
+                            Text(text = "Add")
+                        }
+                    }
+                    LazyColumn{
+                        items(names){ currentName ->
+                            Text(text = currentName)
+                        }
                     }
                 }
             }
         }
-
     }
 }
