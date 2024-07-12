0bb2e8e76a4749946883c8fb95708850a05f4788
Kaleab Beteselassie <54122528+Komputer24@users.noreply.github.com>
2024-07-12 00:15:13 -0400

    Add Dates Tmr

--- app/src/main/java/com/example/scrambleapp/MainActivity.kt
+++ app/src/main/java/com/example/scrambleapp/MainActivity.kt
@@ -3,71 +3,103 @@ package com.example.scrambleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.benchmark.perfetto.Row
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.scrambleapp.ui.theme.ScrambleAppTheme
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import kotlin.math.roundToInt
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.zIndex
+
+@Composable
+private fun DraggableRow(
+    text: String,
+    onRemove: () -> Unit
+) {
+    var offsetY by remember { mutableStateOf(0f) }
+    var initialY by remember { mutableStateOf(0f) }
+    Row(
+        modifier = Modifier
+            .zIndex(if (offsetY != initialY) 1f else 0f)
+            .offset { IntOffset(0, offsetY.roundToInt()) }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        initialY = offsetY
+                    },
+                    onDrag = { change, dragAmount ->
+                        change.consumeAllChanges()
+                        offsetY += dragAmount.y
+                    },
+                    onDragEnd = {
+                        offsetY = initialY
+                    }
+                )
+            }
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color(0xFF7A9E9F))
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Text(
+            text = text,
+            modifier = Modifier.weight(1f).padding(end = 16.dp)
+        )
+        Button(
+            onClick = { onRemove() },
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFE5F55)
+            ),
+            ) {
+                Text(
+                text = "X",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFEEF5DB))
+        )
             ScrambleAppTheme {
-                var name by remember{
-                    mutableStateOf("")
-                }
-                var names by remember{
-                    mutableStateOf(listOf<String>())
-                }
-                Column (
+
+                var name by remember { mutableStateOf("") }
+                var names by remember { mutableStateOf(listOf<String>()) }
+                var miniText1 by remember { mutableStateOf("") }
+                var miniText2 by remember { mutableStateOf("") }
+
+                Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                ){
+                ) {
                     Row(
                         horizontalArrangement = Arrangement.Center,
                         modifier = Modifier.fillMaxWidth(),
@@ -77,76 +109,102 @@ class MainActivity : ComponentActivity() {
                             fontWeight = FontWeight.Bold,
                             fontSize = 30.sp,
                             modifier = Modifier
-                                .padding(top = 30.dp, start = 10.dp)
+                                .padding(top = 30.dp, start = 10.dp),
+                                color = Color(0xFF4F6367)
                         )
                     }
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.Center,
-                    ){
+                    ) {
                         OutlinedTextField(
                             value = name,
-                            onValueChange = {
-                                text -> name = text
-                            },
-                            Modifier
+                            onValueChange = { text -> name = text },
+                            modifier = Modifier
                                 .fillMaxWidth(0.75f)
                                 .padding(10.dp),
                         )
-                        Button(onClick = {
-                                    if(name.isNotBlank()){
-                                        names = names + name
-                                        name = ""
-                                    }
-                                },
-                                shape = RoundedCornerShape(8.dp),
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = Color.LightGray
-                                ),
-                                modifier = Modifier.padding(10.dp).offset(x = -10.dp, y = 0.dp)
-                        ){
+                        Button(
+                            onClick = {
+                                if (name.isNotBlank()) {
+                                    names = names + name
+                                    name = ""
+                                }
+                            },
+                            shape = RoundedCornerShape(8.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFF4F6367)
+                            ),
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .offset(x = -10.dp, y = 0.dp)
+                        ) {
                             Text(
                                 text = "+",
                                 style = TextStyle(fontSize = 33.sp),
+                                color = Color.White
                             )
                         }
-
                     }
-                    LazyColumn {
-                        items(names) { currentName ->
-                            var clickedName by remember { mutableStateOf<String?>(null) }
-                            val isSelected = currentName == clickedName
-                            val backgroundColor = if (isSelected) Color.Yellow else Color.Transparent
-
+//                    Row(
+//                        modifier = Modifier.fillMaxWidth(),
+//                        horizontalArrangement = Arrangement.Center,
+//                    ) {
+//                        OutlinedTextField(
+//                            value = miniText1,
+//                            onValueChange = { text -> miniText1 = text },
+//                            modifier = Modifier
+//                                .weight(1f)
+//                                .padding(10.dp),
+//                        )
+////                        Button(
+////                            onClick = {
+////                                // Do something with miniText1 and miniText2
+////                            },
+////                            shape = RoundedCornerShape(8.dp),
+////                            colors = ButtonDefaults.buttonColors(
+////                                containerColor = Color(0xFF4F6367)
+////                            ),
+////                            modifier = Modifier
+////                                .padding(10.dp)
+////                        ) {
+////                            Text(
+////                                text = "Switch",
+////                                style = TextStyle(fontSize = 15.sp),
+////                                color = Color.White
+////                            )
+////                        }
+//                        OutlinedTextField(
+//                            value = miniText2,
+//                            onValueChange = { text -> miniText2 = text },
+//                            modifier = Modifier
+//                                .weight(1f)
+//                                .padding(10.dp),
+//                        )
+//                    }
+                    LazyColumn (modifier = Modifier.fillMaxSize()){
+                        itemsIndexed(names) { index, currentName ->
+                            DraggableRow(
+                                text = currentName,
+                                onRemove = {
+                                    names = names.toMutableList().apply {
+                                        removeAt(index)
+                                    }
+                                }
+                            )
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .clickable { clickedName = currentName }
-                                    .background(backgroundColor)
-                                    .padding(16.dp),  // Optional: Add padding to the Row
-                                verticalAlignment = Alignment.CenterVertically,  // Center content vertically
-                                horizontalArrangement = Arrangement.SpaceBetween  // Space out the children
                             ) {
-                                Text(
-                                    text = currentName,
+                                Box(
                                     modifier = Modifier
-                                        .weight(1f)  // Make Text take the remaining space
-                                        .padding(end = 16.dp)  // Optional: Add padding to the end of the Text
+                                        .fillMaxWidth() // Fill the width of the Row
+                                        .height(5.dp) // Set a fixed height for the Box
+                                        .background(
+                                            color = Color(0xFFEEF5DB),
+                                        )
                                 )
-                                Button(
-                                    onClick = { names = names.filter { it != currentName } },
-                                    shape = RoundedCornerShape(8.dp),
-                                    colors = ButtonDefaults.buttonColors(
-                                        containerColor = Color.LightGray
-                                    )
-                                ) {
-                                    Text(
-                                        text = "X",
-                                        style = TextStyle(fontSize = 15.sp)
-                                    )
-                                }
                             }
-                            Divider()
                         }
                     }
                 }
